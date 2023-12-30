6f6b7b6e0f4989d2550e686b01878c474cd22681
RafaelNavon <raffi158@gmail.com>
2023-12-30 22:42:36 +0200

    added images, products and responsive design

---
+++ frontend/src/data.js
@@ -0,0 +1,49 @@
+const data = {
+    products: [
+        {
+            name: 'Ralph Lauren shirt',
+            slug: 'ralph-lauren-shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 100,
+            countInStock: 15,
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt',
+        },
+        {
+            name: 'Tommy Hilfiger shirt',
+            slug: 'tommy-hilfiger-shirt',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 110,
+            countInStock: 17,
+            rating: 4.4,
+            numReviews: 12,
+            description: 'very comfy',
+        },
+        {
+            name: 'Ralph Lauren pants',
+            slug: 'ralph-lauren-pants',
+            category: 'Pants',
+            image: '/images/p3.jpg',
+            price: 80,
+            countInStock: 18,
+            rating: 4.7,
+            numReviews: 25,
+            description: 'Definitly worth it',
+        },
+        {
+            name: 'Tommy Hilfiger pants',
+            slug: 'tommy-hilfiger-pants',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 70,
+            countInStock: 24,
+            rating: 3.9,
+            numReviews: 10,
+            description: 'pretty good',
+        },
+    ]
+}
+export default data;
