01bcc285a456c719d2ca7ed345bdeefbd48fa80d
RafaelNavon <raffi158@gmail.com>
2024-09-16 03:37:38 +0300

    added a drop down Nav for user, and an invalid error message

--- backend/utils.js
+++ backend/utils.js
@@ -2,7 +2,12 @@ import jwt from "jsonwebtoken";
 
 export const generateToken = (user) => {
   return jwt.sign(
-    { id: user.id, name: user.name, email: user.email, isAdmin: user.isAdmin },
+    {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    },
     process.env.JWT_SECRET,
     {
       expiresIn: "30d",
