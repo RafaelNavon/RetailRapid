8f542c8a7c9aa07e2c5bee0fe1d60d9dbdd5cb82
RafaelNavon <raffi158@gmail.com>
2024-11-15 03:00:15 +0200

    updated server.js, inserted more products after scraping

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,37 @@ import seedRouter from "./routes/seedRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
-
+import path from "path";
+import fs from "fs";
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("connected to db");
+    fs.readFile("products.json", (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      // Parse the JSON data
+      const products = JSON.parse(data);
+
+      // Get a reference to the MongoDB database
+      const db = mongoose.connection;
+      // Access the 'products' collection in the database
+      const collection = db.collection("products");
+
+      // Insert each product into the 'products' collection
+      products.forEach((product) => {
+        collection.insertOne(product, function (err) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+        });
+      });
+    });
   })
   .catch((err) => {
     console.log(err.message);
